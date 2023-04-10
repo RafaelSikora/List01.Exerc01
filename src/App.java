1f310ae4e01b66a1610ed4823aedadd58cb2414b
Rafael Sikora <127799119+RafaelSikora@users.noreply.github.com>
2023-04-09 21:47:49 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 class Main {
-  Exercicio 01: Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+  //Exercicio 01: Faça um Programa que mostre a mensagem "Alo mundo" na tela.
   public static void main(String[] args) {
     System.out.println("Alô mundo!");
   }
